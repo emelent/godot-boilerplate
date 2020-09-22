e0a95caeb0d07c3821a205216d539f286f672ace
Roy <roy.motloutsi@gmail.com>
2020-09-22 18:25:37 +0200

    feat(screen_shake): add screen shake

--- scenes/menus/main_menu/main_menu.gd
+++ scenes/menus/main_menu/main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 func _on_Quit_pressed():
@@ -11,3 +11,7 @@ func _on_Controls_pressed():
 
 func _on_Play_pressed():
 	GameManager.goToScene(Global.scenes.level_test)
+
+func _on_Shake_pressed():
+	$Camera/ScreenShaker.start(5, 20, 5)
+		
